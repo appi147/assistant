d26034aaf866f22f8384c42da1901ae7b46d147e
appi <arpitkumar147@gmail.com>
2017-10-23 16:01:23 +0530

    Basic app file

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import json
+import requests
+from flask import Flask, request, render_template
+
+
+@app.route('/', methods=['GET'])
+def verify():
+    if request.args.get("hub.mode") == "subscribe"and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    # return "Assistant", 200
+    return render_template('index.html'), 200
