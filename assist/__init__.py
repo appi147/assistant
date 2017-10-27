cc876090f4dfc0c7474b2d3d159a2fbbe3a3e3a4
appi <arpitkumar147@gmail.com>
2017-10-27 15:11:47 +0530

    General bug fix
    AttributeError: 'dict' object has no attribute 'json'

--- assist/__init__.py
+++ assist/__init__.py
@@ -2,9 +2,8 @@ import json
 
 
 def handler(entities, text):
-    entities = entities.json()
     response = []
     for entity in entities:
-        if entity == 'greetings' and entity['confidence'] >= 0.8:
+        if entity == 'greetings' and entities['greetings']['confidence'] >= 0.8:
             response.append('greetings')
     return response
