efd97b1bd999e12be456c32e4ad011055715eb40
appi147 <arpitkumar147@gmail.com>
2017-12-25 19:00:17 +0530

    Refactor code
    Initially FB rejected some messages for word limits

--- modules/cric.py
+++ modules/cric.py
@@ -1,25 +1,29 @@
-import re
 from pycricbuzz import Cricbuzz
 
 
 c = Cricbuzz()
 
 
-def cleanhtml(raw_html):
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', raw_html)
-    return cleantext
-
-
-def commentary(id):
+def cleanhtml(text):
     resp = []
-    data = c.commentary(id)
-    for lines in data['commentary']:
-        resp.append(cleanhtml(lines))
+    text = text.split('<br/>')
+    for i in text:
+        if len(i):
+            resp.append(i)
     return resp
 
-def live_score(desc):
-    mid = match_id(desc)
+
+def commentary(mid):
+    resp = []
+    data = c.commentary(mid)
+    for lines in data['commentary']:
+        resp.extend(cleanhtml(lines))
+    return resp
+
+
+# Credits to umangahuja1
+def live_score(mid):
+    resp = []
     data = c.livescore(mid)
     score = {}
     score['matchinfo'] = "{}, {}".format(data['matchinfo']['mnum'], data['matchinfo']['mchdesc'])
@@ -28,54 +32,59 @@ def live_score(desc):
     score['batting'] = data['batting']
 
     text = ''
-    text += score['matchinfo'] + '\n' + score['status'] + '\n\n'
+    text += score['matchinfo'] + '\n' + score['status'] + '\n'
     text += score['batting']['team'] + '\n'
+    resp.append(text)
 
+    text = ''
     for scr in reversed(score['batting']['score']):
         text += "{} :- {}/{} in {} overs\n".format(scr['desc'], scr['runs'], scr['wickets'], scr['overs'])
     for b in reversed(score['batting']['batsman']):
         text += "{} : {}({}) \n".format(b['name'].strip('*'), b['runs'], b['balls'])
     text += "\n" + score['bowling']['team'] + '\n'
+    resp.append(text)
+
+    text = ''
     for scr in reversed(score['bowling']['score']):
         text += "{} :- {}/{} in {} overs\n".format(scr['desc'], scr['runs'], scr['wickets'], scr['overs'])
     for b in reversed(score['bowling']['bowler']):
         text += "{} : {}/{} \n".format(b['name'].strip('*'), b['wickets'], b['runs'])
-    return text
+    return resp
 
 
-def scorecard(id):
-    data = c.scorecard(id)
+# Credits to umangahuja1
+def scorecard(mid):
+    resp = []
+    data = c.scorecard(mid)
     card = {}
     card['matchinfo'] = "{}, {}".format(data['matchinfo']['mnum'], data['matchinfo']['mchdesc'])
     card['status'] = "{}, {}".format(data['matchinfo']['mchstate'].title(), data['matchinfo']['status'])
     card['scorecard'] = data['scorecard']
     text = ''
-    text += card['matchinfo'] + '\n' + card['status'] + '\n\n'
-    text += '*' * 35 + '\n\n'
+    text += card['matchinfo'] + '\n' + card['status'] + '\n'
+    resp.append(text)
 
     for scr in reversed(card['scorecard']):
-        text += "{} {}\n{}/{} in {} overs\n\n".format(scr['batteam'], scr['inngdesc'], scr['runs'], scr['wickets'],
-                                                                            scr['overs'])
+        text = ''
+        text += "{} {}\n{}/{} in {} overs\n\n".format(scr['batteam'], scr['inngdesc'], scr['runs'], scr['wickets'], scr['overs'])
         text += "Batting\n"
-        text += "{:<17} {:<3} {:<3} {:<3} {}\n\n".format('Name', 'R', 'B', '4', '6')
+        text += "{:<17} {:<3} {:<3} {:<3} {}\n".format('Name', 'R', 'B', '4', '6')
         for b in scr['batcard']:
-            text += "{:<17} {:<3} {:<3} {:<3} {}\n{}\n\n".format(b['name'], b['runs'], b['balls'], b['fours'], b['six'],
-                                                                              b['dismissal'])
-        text += "-" * 35 + "\n\n"
+            text += "{:<17} {:<3} {:<3} {:<3} {}\n{}\n".format(b['name'], b['runs'], b['balls'], b['fours'], b['six'], b['dismissal'])
+        text += "-" * 35 + "\n"
         text += "Bowling\n"
         text += "{:<17} {:<5} {:<3} {:<3} {}\n\n".format('Name', 'O', 'M', 'R', 'W')
         for b in scr['bowlcard']:
-            text += "{:<17} {:<5} {:<3} {:<3} {}\n\n".format(b['name'], b['overs'], b['maidens'], b['runs'],
-                                                                          b['wickets'])
-        text += '*' * 35 + '\n\n'
-    return text
+            text += "{:<17} {:<5} {:<3} {:<3} {}\n\n".format(b['name'], b['overs'], b['maidens'], b['runs'], b['wickets'])
+        resp.append(text)
+    return resp
 
 
 def preview():
     resp = []
     current_matches = c.matches()
     for match in current_matches:
-        if match['mchstate']=='preview':
+        if match['mchstate'] == 'preview':
             resp.append(match['srs'])
             resp.append(match['mchdesc'])
             resp.append(match['mnum'])
@@ -83,13 +92,13 @@ def preview():
             resp.extend(commentary(match['id']))
     return resp
 
-    
+
 def result():
     resp = []
     current_matches = c.matches()
     for match in current_matches:
-        if match['mchstate']=='Result':
-            resp.append(scorecard(match['id']))
+        if match['mchstate'] == 'Result':
+            resp.extend(scorecard(match['id']))
     return resp
 
 
@@ -97,11 +106,10 @@ def live():
     resp = []
     current_matches = c.matches()
     for match in current_matches:
-        if match['mchstate']=='live':
+        if match['mchstate'] == 'live':
             resp.append(live_score(match['id']))
     return resp
 
 if __name__ == '__main__':
     for r in result():
         print(r)
-
